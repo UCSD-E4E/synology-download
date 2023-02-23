29efdefafb207048c4754a5cbf6dd4d85d4b91fe
Nathan Hui <nthui@eng.ucsd.edu>
2023-02-22 16:07:59 -0800

    Added CI support

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,19 +1,41 @@
 """Test Download
 """
 import json
+import os
 from pathlib import Path
 from tempfile import TemporaryDirectory
+from typing import Dict
+
+import pytest
 
 from NasDownload import nas
 
 
-def test_download():
-    with open('credentials.json', 'r', encoding='ascii') as handle:
-        creds = json.load(handle)
+@pytest.fixture(name='creds')
+def create_creds() -> Dict[str, str]:
+    """Obtains the credentials
+
+    Returns:
+        Dict[str, str]: Username and password dictionary
+    """
+    if Path('credentials.json').is_file():
+        with open('credentials.json', 'r', encoding='ascii') as handle:
+            return json.load(handle)
+    else:
+        value = os.environ['NAS_CREDS'].splitlines()
+        assert len(value) == 2
+        return {
+            'username': value[0],
+            'password': value[1]
+        }
+
+def test_download(creds):
+    """Tests downloading a file
+    """
     with TemporaryDirectory() as tmpdir:
         nas.nas_unzip(
             network_path='smb://e4e-nas.ucsd.edu/temp/github_actions/synology-download/test.zip',
-            output_path=tmpdir,
+            output_path=Path(tmpdir),
             username=creds['username'],
             password=creds['password']
         )
